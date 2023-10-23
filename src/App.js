24b86ad662eb03e4266093b21c4fd35c6898840c
diannedaian <142431216+diannedaian@users.noreply.github.com>
2023-10-23 11:28:08 -0400

    made 9 boxes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function Square(){
+  
+
+  return( 
+    <>
+    <div classname = "board-row">
+    <button classname = "square">1</button> 
+  <button classname = "square">2</button> 
+  <button classname = "square">3</button> 
     </div>
+
+    <div classname = "board-row">
+    <button classname = "square">4</button> 
+  <button classname = "square">5</button> 
+  <button classname = "square">6</button> 
+    </div>
+
+    <div classname = "board-row">
+    <button classname = "square">7</button> 
+  <button classname = "square">8</button> 
+  <button classname = "square">9</button> 
+    </div>
+
+  </>
   );
+  
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           I am going to make tic tac toe. 
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
